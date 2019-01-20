adad2ed2ad26e2bdb80c38904f9d40b08ece1e3a
zeroheartza <zeroheartza@gmail.com>
2019-01-21 05:41:50 +0700

    lab

---
+++ lab1/hw1.java
@@ -0,0 +1,19 @@
+import java.util.Random;
+
+public class hw1{
+  public static void main(String[] args){
+
+
+    Random rand = new Random();
+    double a = rand.nextDouble()*50;
+    double b = rand.nextDouble()*50;
+    double c = a+b;
+    System.out.println("Sum of "+a+" + "+b+" = "+c+"\n");
+    if(c>=50){
+      System.out.println("Value = PASS");
+    }
+    else{
+      System.out.println("Value = NOTPASS");
+    }
+  }
+}
