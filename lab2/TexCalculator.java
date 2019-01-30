2550ae441705ae86bf04dd76f51ca3e549eaf7b3
zeroheartza <zeroheartza@gmail.com>
2019-01-30 11:57:01 +0700

    lab

--- lab2/TexCalculator.java
+++ lab2/TexCalculator.java
@@ -1,8 +1,9 @@
+
 import java.util.*;
 
 
-;public class TexCalculator{
-    public static int calulator(int Tex) {
+public class TexCalculator{
+    public static int cal_1(int Tex) {
         if(Tex>5000000){
             return 35;
         }
@@ -29,37 +30,10 @@ import java.util.*;
         }
         
     }
-    public static int cal_tex(int Tex) {
-        if(Tex>5000000){
-            return Tex-5000000;
-        }
-        if(Tex>2000000){
-            return Tex-2000000;
-        }
-        if(Tex>1000000){
-            return Tex-1000000;
-        }
-        if(Tex>750000){
-            return Tex-750000;
-        }
-        if(Tex>500000){
-            return Tex-500000;
-        }
-        if(Tex>300000){
-            return Tex-300000;
-        }
-        if(Tex>150000){
-            return Tex-150000;
-        }
-        else{
-            return 0;
-        }
-        
-    }
 
     public static int Step(int Tex) {
-        if(Tex==35){
-            return 7;
+        if(Tex==7){
+            return 35;
         }
         if(Tex==30){
             return 6;
@@ -84,59 +58,138 @@ import java.util.*;
         }
         
     }
+    public static int cal_2(int sum){
 
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        System.out.print("t");
-        int Salary;
-        int Yc;
-        int Td;
-        int Oi;
-        int Income;
-        int Tex;
-        int sTex;
-        String choose;
-        int S;
-        int x;
-        System.out.print("Enter Salary : ");
-        Salary=input.nextInt();
-        System.out.print("Enter Year cost : ");
-        Yc=input.nextInt();
-        System.out.print("Enter Tax deduction : ");
-        Td=input.nextInt();
-        System.out.print("\nYou have other income? ");
-        choose=input.nextLine();
-        choose=input.nextLine();
-        if(choose.equals("Y")){
-            System.out.print("Input other income : ");
-            Oi=input.nextInt();
-            Income = (Salary*12)-Yc - Td + Oi;
-            System.out.println("Income : "+"("+Salary+"x"+"12)"+" - "+Yc +" - "+ Td +" + "+Oi+" = "+Income);
-            Tex = calulator(Income);
-            System.out.print("Tex is "+Tex+"%");
-            S=Step(Tex);
-            Tex=Income;
-            for(int i=0;i<S;i++){
-                System.out.println(Tex);
-                System.out.println("Step "+(S-i));
-                x=calulator(Tex);
-                Tex = cal_tex(Tex);
-                System.out.println(Tex+" x "+x+" = "+((Tex*x)/100));
-            }
-            
+        int n =Step(cal_1(sum));
+        
+        System.out.print(sum);
+        
+        switch (n) {
+            case 7:
+                System.out.print(" - 3000000");
+                sum-=3000000;
+                System.out.print(" = "+sum+" ");
+  
+            case 6:
+                System.out.print(" - 1000000");
+                sum-=1000000;
+                System.out.print(" = "+sum+" ");
 
+            case 5:
+                System.out.print("  250000 ");
+                sum-=250000;
+                System.out.print(" = "+sum+" ");
+                
+            case 4:
+                System.out.print(" - 250000 ");
+                sum-=250000;
+                System.out.print(" = "+sum+" ");
+       
+            case 3:
+                System.out.print(" - 200000 ");
+                sum-=200000;
+                System.out.print(" = "+sum+" ");
+
+            case 2:
+                System.out.print(" - 150000 ");
+                sum-=150000;
+                System.out.print(" = "+sum+" ");
+
+            case 1:
+                System.out.print(" - 150000 ");
+                sum-=150000;
+                System.out.print(" = "+sum+" ");   
+        }
+        return sum;
+    }
+
+    public static int cal_3(int Tex){
+        if(Tex==35){
+            return 7;
+        }
+        if(Tex==6){
+            return 30;
+        }
+        if(Tex==5){
+            return 25;
+        }
+        if(Tex==4){
+            return 20;
+        }
+        if(Tex==3){
+            return 15;
+        }
+        if(Tex==2){
+            return 10;
+        }
+        if(Tex==1){
+            return 5;
         }
         else{
-           
-            Income = (Salary*12)-Yc - Td;
-            System.out.println("Income : "+"("+Salary+"x"+"12)"+" - "+Yc +" - "+ Td +" = "+Income);
-            Tex = calulator(Income);
-            x=calulator(Tex);
-            System.out.print("Tex is "+Tex+"%");
-            S=Step(Tex);
-            System.out.println("Step "+S);
-            Tex = cal_tex(Income);
+            return 0;
         }
         
+    }
+
+    
+    
+
+    public static void main(String[] args){
+        
+        int income;
+        Scanner inStr = new Scanner(System.in);
+        Scanner inInt = new Scanner(System.in);
+        System.out.print("Enter Salary : ");
+        int Salary=inInt.nextInt();
+        System.out.print("Enter Year cost : ");
+        int YearC=inInt.nextInt();
+        System.out.print("Enter Tax deduction : ");
+        int TaxD=inInt.nextInt();
+        System.out.print("\nYou have other income? ");
+        String choose=inStr.nextLine();
+        
+        if(choose.equals("Y")){
+            System.out.print("Input other income : ");
+            int Oincome=inInt.nextInt();
+            income = (Salary*12)-YearC - TaxD + Oincome;
+            System.out.println("\nIncome : "+"("+Salary+"x"+"12)"+" - "+YearC +" - "+ TaxD +" + "+Oincome+" = "+income);
+            System.out.println("Tax is "+cal_1(income)+"%\n");
+        }
+        else{
+            income = (Salary*12)-YearC - TaxD ;
+            System.out.println("\nIncome : "+"("+Salary+"x"+"12)"+" - "+YearC +" - "+ TaxD +" = "+income);
+            System.out.println("Tax is "+cal_1(income)+"%\n");
+
+        }
+        if(cal_1(income)!=0){
+            int sum=0;
+            int[] List = {150000, 150000, 200000, 250000,250000,1000000,3000000};
+            int n =Step(cal_1(income));
+            if(n==1){
+                System.out.print("Cal Tax ");
+                int x= cal_2(income);
+                System.out.print(" * "+cal_1(income)+"%"+" = ");
+                System.out.println(x*cal_1(income)/100);
+                sum+=x*cal_1(income)/100;
+            }
+            else{
+                System.out.println("step "+n);
+                System.out.print("Cal Tax ");
+                int x= cal_2(income);
+                System.out.print(" * "+cal_1(income)+"%"+" = ");
+                System.out.println(x*cal_1(income)/100);
+                sum+=x*cal_1(income)/100;
+                
+                for(int i=n-1;i>0;i--){
+                    System.out.println("step "+i);
+                    System.out.println("Cal Tax "+List[i]+" * "+cal_3(i)+"% = "+(List[i]*cal_3(i))/100);
+                    sum+=(List[i]*cal_3(i))/100;
+                }
+            }
+            System.out.println("Your Tax = "+sum+" BTH");
+        }
+    else{
+        System.out.println("Your Tax = Free Tax");
+        }
     }
 }
